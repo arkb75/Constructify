5750d1d0060872c5052892d6a10e7cc7c8f617db
arkb75 <rafaykhurram@live.com>
2024-07-12 13:36:39 -0700

    Test Primevue Page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,29 +2,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import PrimeVue and related components
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'; // theme
+import 'primevue/resources/primevue.min.css'; // core css
+import 'primeicons/primeicons.css'; // icons
+import 'primeflex/primeflex.css'; // PrimeFlex
+
 import jQuery from './assets/js/jquery.min.js'
 window.$ = window.jQuery = jQuery
 
-//import "http://code.jquery.com/jquery-1.11.3.min.js";
+// Import additional JS libraries
 import './assets/js/jquery.min.js';
 import './assets/js/popper.min.js';
 import "./assets/js/bootstrap.min.js";
 import "./assets/js/slick.min.js";
-
-//import "./assets/js/jquery.waypoints.min.js";
 import "./assets/js/jquery.easing.1.3.js";
 import "./assets/js/jquery.mb.YTPlayer.min.js";
-
 import "./assets/js/main.js";
 
-// import './assets/css/bootstrap/bootstrap.css';
-// import './assets/fonts/ionicons/css/ionicons.min.css';
-// //import './assets/fonts/law-icons/font/flaticon.css';
-// import './assets/fonts/fontawesome/css/font-awesome.min.css';
-// import './assets/css/slick.css';
-// import './assets/css/slick-theme.css';
-// import './assets/css/helpers.css';
-// import './assets/css/style.css';
-// import './assets/css/landing-2.css';
-
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue)
+  .mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
   component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+},
+{
+  path: '/test',
+  name: 'test',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Test.vue')
 }
 ]
 
